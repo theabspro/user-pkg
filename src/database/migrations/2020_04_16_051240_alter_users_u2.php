7dea2d9f6a6a7204cb941c0267da89565fef6ac7
saravana <40257392+saravanacse1996@users.noreply.github.com>
2020-04-16 12:13:11 +0530

    fix

--- src/database/migrations/2020_04_16_051240_alter_users_u2.php
+++ src/database/migrations/2020_04_16_051240_alter_users_u2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterUsersU1 extends Migration
+class AlterUsersU2 extends Migration
 {
     /**
      * Run the migrations.
