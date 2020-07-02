ae289ed1fd8f22a9f942654378dd7151bcbc89e0
Abdul <abdul@uitouxsolutions.com>
2020-07-02 21:20:56 +0530

    Update User.php

--- src/models/User.php
+++ src/models/User.php
@@ -4,7 +4,6 @@ namespace Abs\UserPkg;
 
 use Abs\HelperPkg\Traits\SeederTrait;
 use App\Company;
-use App\Config;
 use App\Role;
 use DB;
 use Hash;
@@ -273,40 +272,40 @@ class User extends Authenticatable {
 
 	// Static Operations --------------------------------------------------------------
 
-	public static function createFromObject($record_data) {
+	// public static function createFromObject($record_data) {
 
-		$errors = [];
-		$company = Company::where('code', $record_data->company)->first();
-		if (!$company) {
-			dump('Invalid Company : ' . $record_data->company);
-			return;
-		}
+	// 	$errors = [];
+	// 	$company = Company::where('code', $record_data->company)->first();
+	// 	if (!$company) {
+	// 		dump('Invalid Company : ' . $record_data->company);
+	// 		return;
+	// 	}
 
-		$admin = $company->admin();
-		if (!$admin) {
-			dump('Default Admin user not found');
-			return;
-		}
+	// 	$admin = $company->admin();
+	// 	if (!$admin) {
+	// 		dump('Default Admin user not found');
+	// 		return;
+	// 	}
 
-		$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
-		if (!$type) {
-			$errors[] = 'Invalid Tax Type : ' . $record_data->type;
-		}
+	// 	$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
+	// 	if (!$type) {
+	// 		$errors[] = 'Invalid Tax Type : ' . $record_data->type;
+	// 	}
 
-		if (count($errors) > 0) {
-			dump($errors);
-			return;
-		}
+	// 	if (count($errors) > 0) {
+	// 		dump($errors);
+	// 		return;
+	// 	}
 
-		$record = static::firstOrNew([
-			'company_id' => $company->id,
-			'name' => $record_data->tax_name,
-		]);
-		$record->type_id = $type->id;
-		$record->created_by_id = $admin->id;
-		$record->save();
-		return $record;
-	}
+	// 	$record = static::firstOrNew([
+	// 		'company_id' => $company->id,
+	// 		'name' => $record_data->tax_name,
+	// 	]);
+	// 	$record->type_id = $type->id;
+	// 	$record->created_by_id = $admin->id;
+	// 	$record->save();
+	// 	return $record;
+	// }
 
 	public static function mapRoles($records, $company = null, $specific_company = null, $tc) {
 		$success = 0;
