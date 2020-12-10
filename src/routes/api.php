4dafcbedf2aea3dad063a581733626a2131ac461
Vijay-UITOUX <52068840+Vijay-UITOUX@users.noreply.github.com>
2020-12-10 17:57:27 +0530

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,11 +2,11 @@
 
 use App\Http\Controllers\Api\Masters\Auth\UserController;
 
-Route::group(['middleware' => ['api'], 'prefix' => '/api/master/auth/user'], function () {
+Route::group(['middleware' => ['auth:api'], 'prefix' => '/api/masters/auth/user'], function () {
 	$className = UserController::class;
 	Route::get('index', $className . '@index');
 	Route::get('read/{id}', $className . '@read');
 	Route::post('save', $className . '@save');
 	Route::get('options', $className . '@options');
-	Route::get('delete/{User}', $className . '@delete');
+	Route::get('delete/{user}', $className . '@delete');
 });
